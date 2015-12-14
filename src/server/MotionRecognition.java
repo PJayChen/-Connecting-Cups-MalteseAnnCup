9bd4e545de9de3e4dc8150fa74d9e00fe6a51e09
pjaychen <n26021539@mail.ncku.edu.tw>
2015-12-14 14:57:12 +0800

    add basic motion recognition function

--- src/server/MotionRecognition.java
+++ src/server/MotionRecognition.java
@@ -10,11 +10,17 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.BlockingQueue;
 
+import net.sf.javaml.core.DenseInstance;
+import net.sf.javaml.core.Instance;
+import net.sf.javaml.distance.fastdtw.dtw.FastDTW;
+import net.sf.javaml.distance.fastdtw.timeseries.TimeSeries;
+
 public class MotionRecognition extends Thread {
 
 	private static final boolean DEBUG_PARSING = false;
-
-	private static final boolean DEBUG_MOTION_DETECTION = true;
+	private static final boolean DEBUG_MOTION_DETECTION = false;
+	private static final boolean DEBUG_SIMILARITY = false;
+	private static final boolean DEBUG_IDENTIFY_MOTION = true;
 
 	private static final boolean TRAINING_PHASE = true;
 
@@ -30,8 +36,9 @@ public class MotionRecognition extends Thread {
 	private static final int MOTION_DETECTION = 1;
 	private static final int FIND_END_FRAME = 2;
 	private static final int ANALYSIS_FRAMES = 3;
+	private static final int MOTION_RECOGNITION = 4;
 	private int state = MOTION_DETECTION;
-	
+
 	private static final int THRESHOLD_MOTION = 50;
 
 	private List<List<FeatureVector>> motionFramesList;
@@ -48,7 +55,8 @@ public class MotionRecognition extends Thread {
 	}
 
 	private void parsingRawDataStream(String stream) {
-		int size = featureVectorForPlot.size(); // store the last size of accel data
+		int size = featureVectorForPlot.size(); // store the last size of accel
+												// data
 		if (DEBUG_PARSING)
 			System.out.print(stream);
 		String[] splitedStream = stream.split(";");
@@ -63,10 +71,27 @@ public class MotionRecognition extends Thread {
 						int y = Integer.valueOf(accelStr[1]);
 						int x = Integer.valueOf(accelStr[0]);
 						int magnitude = (int) (Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2)) - 64);
-						int norm = (int) (Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2)));
-						
+						// int norm = (int) (Math.sqrt(Math.pow(x, 2) +
+						// Math.pow(y, 2) + Math.pow(z, 2)));
+						// double average = (x + y + z) / 3;
+						// double standardDeviation = (Math
+						// .sqrt((Math.pow(x - average, 2) + Math.pow(y -
+						// average, 2) + Math.pow(z - average, 2))
+						// / (3 - 1)));
+						double average = (x + y + z + magnitude) / 4;
+						double standardDeviation = (Math.sqrt((Math.pow(x - average, 2) + Math.pow(y - average, 2)
+								+ Math.pow(z - average, 2) + Math.pow(magnitude - average, 2)) / (4 - 1)));
+						int zScore_x = (int) ((x - average) / standardDeviation * 10);
+						int zScore_y = (int) ((y - average) / standardDeviation * 10);
+						int zScore_z = (int) ((z - average) / standardDeviation * 10);
+						int zScore_m = (int) ((magnitude - average) / standardDeviation * 10);
+
 						// store parsed data into a List for plot chart
-						featureVectorForPlot.add(new FeatureVector(x,y,z,magnitude));
+						// featureVectorForPlot.add(new
+						// FeatureVector(x,y,z,magnitude));
+						// featureVectorForPlot.add(new FeatureVector(zScore_x,
+						// zScore_y, zScore_z, magnitude));
+						featureVectorForPlot.add(new FeatureVector(zScore_x, zScore_y, zScore_z, zScore_m));
 
 						// store parsed data into processing List
 						featureVectorList.add(new FeatureVector(x, y, z, magnitude));
@@ -92,8 +117,8 @@ public class MotionRecognition extends Thread {
 		}
 
 	}
-    
-	//Power = 1/N * Sum(f(n))^2
+
+	// Power = 1/N * Sum(f(n))^2
 	private int getPowerOfFrame(List<FeatureVector> featureVectorList) {
 		int power = 0;
 
@@ -106,8 +131,8 @@ public class MotionRecognition extends Thread {
 		return power;
 	}
 
-	//Framing sample sequence.
-	//64 samples per frame, 50% overlap
+	// Framing sample sequence.
+	// 64 samples per frame, 50% overlap
 	private List<FeatureVector> getFrame(List<FeatureVector> featureVectorList) {
 		List<FeatureVector> frameFV = new LinkedList<FeatureVector>();
 
@@ -122,10 +147,11 @@ public class MotionRecognition extends Thread {
 		return frameFV;
 	}
 
-	//Detect whether active motion or not 
+	// Detect whether active motion or not
 	private boolean motionDetection(List<FeatureVector> featureVectorList) {
 
-		System.out.printf("Frame[%d]: frame size is %d, ", frameIndex, featureVectorList.size());
+		if (DEBUG_MOTION_DETECTION)
+			System.out.printf("Frame[%d]: frame size is %d, ", frameIndex, featureVectorList.size());
 
 		int power = getPowerOfFrame(featureVectorList);
 
@@ -139,6 +165,127 @@ public class MotionRecognition extends Thread {
 			return false;
 	}
 
+	private double getSimilarity(List<List<FeatureVector>> templateFeatureFrameList,
+			List<List<FeatureVector>> testingFeatureFrameList) {
+
+		// ----- extract feature vectors from templateFeatureFrameList -----
+		double[] d_x = new double[templateFeatureFrameList.size() * templateFeatureFrameList.get(0).size()],
+				d_y = new double[templateFeatureFrameList.size() * templateFeatureFrameList.get(0).size()],
+				d_z = new double[templateFeatureFrameList.size() * templateFeatureFrameList.get(0).size()],
+				d_m = new double[templateFeatureFrameList.size() * templateFeatureFrameList.get(0).size()];
+
+		// extract features frame by frame
+		for (int j = 0; j < templateFeatureFrameList.size(); j++) {
+			// extracting features from each frame
+			for (int i = 0; i < templateFeatureFrameList.get(j).size(); i++) {
+				d_x[j * templateFeatureFrameList.get(j).size() + i] = (double) templateFeatureFrameList.get(j).get(i)
+						.getAcceleration_x();
+				d_y[j * templateFeatureFrameList.get(j).size() + i] = (double) templateFeatureFrameList.get(j).get(i)
+						.getAcceleration_y();
+				d_z[j * templateFeatureFrameList.get(j).size() + i] = (double) templateFeatureFrameList.get(j).get(i)
+						.getAcceleration_z();
+				d_m[j * templateFeatureFrameList.get(j).size() + i] = (double) templateFeatureFrameList.get(j).get(i)
+						.getAcceleration_magnitude();
+			}
+		}
+		// if (DEBUG_SIMILARITY) {
+		// for (int i=0;i<d_x.length;i++)
+		// System.out.println("[" + d_x[i] + ", " + d_y[i] + ", " + d_z[i] + ",
+		// " + d_m[i] + "], ");
+		// System.out.println("\n");
+		// }
+		Instance templateInstance_x = new DenseInstance(d_x);
+		Instance templateInstance_y = new DenseInstance(d_y);
+		Instance templateInstance_z = new DenseInstance(d_z);
+		Instance templateInstance_m = new DenseInstance(d_m);
+		// -----------------------------------------------------------------
+
+		// ----- extract feature vectors from testingFeatureFrameList -----
+		d_x = new double[testingFeatureFrameList.size() * testingFeatureFrameList.get(0).size()];
+		d_y = new double[testingFeatureFrameList.size() * testingFeatureFrameList.get(0).size()];
+		d_z = new double[testingFeatureFrameList.size() * testingFeatureFrameList.get(0).size()];
+		d_m = new double[testingFeatureFrameList.size() * testingFeatureFrameList.get(0).size()];
+
+		// extract features frame by frame
+		for (int j = 0; j < testingFeatureFrameList.size(); j++) {
+			// extracting features from each frame
+			for (int i = 0; i < testingFeatureFrameList.get(j).size(); i++) {
+				d_x[j * testingFeatureFrameList.get(j).size() + i] = (double) testingFeatureFrameList.get(j).get(i)
+						.getAcceleration_x();
+				d_y[j * testingFeatureFrameList.get(j).size() + i] = (double) testingFeatureFrameList.get(j).get(i)
+						.getAcceleration_y();
+				d_z[j * testingFeatureFrameList.get(j).size() + i] = (double) testingFeatureFrameList.get(j).get(i)
+						.getAcceleration_z();
+				d_m[j * testingFeatureFrameList.get(j).size() + i] = (double) testingFeatureFrameList.get(j).get(i)
+						.getAcceleration_magnitude();
+			}
+		}
+		// if (DEBUG_SIMILARITY) {
+		// for (int i=0;i<d_x.length;i++)
+		// System.out.println("[" + d_x[i] + ", " + d_y[i] + ", " + d_z[i] + ",
+		// " + d_m[i] + "], ");
+		// System.out.println("\n");
+		// }
+
+		Instance testingInstance_x = new DenseInstance(d_x);
+		Instance testingInstance_y = new DenseInstance(d_y);
+		Instance testingInstance_z = new DenseInstance(d_z);
+		Instance testingInstance_m = new DenseInstance(d_m);
+		// -----------------------------------------------------------------
+
+		// ----- calculate distance by fastDTW -----
+		double dist = FastDTW.getWarpDistBetween(new TimeSeries(templateInstance_x), new TimeSeries(testingInstance_x),
+				30)
+				+ FastDTW.getWarpDistBetween(new TimeSeries(templateInstance_y), new TimeSeries(testingInstance_y), 30)
+				+ FastDTW.getWarpDistBetween(new TimeSeries(templateInstance_z), new TimeSeries(testingInstance_z), 30)
+				+ FastDTW.getWarpDistBetween(new TimeSeries(templateInstance_m), new TimeSeries(testingInstance_m), 30);
+
+		if (DEBUG_SIMILARITY) {
+			System.out.printf("Similarity: %d\n", (int) dist);
+		}
+		// -----------------------------------------
+
+		return dist;
+	}
+
+	private void identifyMotion(String templateName) {
+
+		try {
+			// Read activity motion frames from the file to check
+			// whether store correctly.
+			FileInputStream fis = new FileInputStream("./dataset/motion_" + templateName);
+			ObjectInputStream ois;
+			ois = new ObjectInputStream(fis);
+			List<List<FeatureVector>> mList = (List<List<FeatureVector>>) ois.readObject();
+			ois.close();
+			fis.close();
+			/*if (DEBUG_IDENTIFY_MOTION) {
+				System.out.printf("[Read from file] ");
+				System.out.printf("Number of frame is %d \n", mList.size());
+				for (List<FeatureVector> fv : mList) {
+					for (int i = 0; i < fv.size(); i++) {
+						System.out.printf("[%d, %d, %d, %d], ", fv.get(i).getAcceleration_x(),
+								fv.get(i).getAcceleration_y(), fv.get(i).getAcceleration_z(),
+								fv.get(i).getAcceleration_magnitude());
+					}
+					System.out.println("\n");
+				}
+			}*/
+			
+			//compare run-time motion frames with template 
+			double dist = getSimilarity(mList, motionFramesList);
+			if (DEBUG_IDENTIFY_MOTION) {
+				System.out.printf("Motion %s, similarity: %d\n", templateName, (int)dist);
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+	}
+
 	@Override
 	public void run() {
 		// ----- Setup chart -----
@@ -150,43 +297,45 @@ public class MotionRecognition extends Thread {
 				// ----- take data from the queue and parse it. -----
 				parsingRawDataStream(rawDataStreamQueue.take());
 
-				// ----- Plot data -----				
+				// ----- Plot data -----
 				// Increase time axis
 				while (axis_x.size() < featureVectorForPlot.size())
 					axis_x.add(axis_x.get(axis_x.size() - 1) + 10);
 				// Limit the total number of points
-				while (axis_x.size() > 200  && featureVectorForPlot.size() > 200) {
+				while (axis_x.size() > 200 && featureVectorForPlot.size() > 200) {
 					axis_x.remove(0);
 					featureVectorForPlot.remove(0);
 				}
-					
-				//update chart
+
+				// update chart
 				realtimeChart.updateChart(axis_x, featureVectorForPlot);
 				// ----------------------
 
-//				if (DEBUG_MOTION_DETECTION) {
-//					System.out.printf("State [%d] \n\n", state);
-//				}
+				// if (DEBUG_MOTION_DETECTION) {
+				// System.out.printf("State [%d] \n\n", state);
+				// }
 				// ----- processing data -----
 				switch (state) {
 				case MOTION_DETECTION:
 					if (featureVectorList.size() >= 64) {
-                        //get a frame from input sequence of samples
+						// get a frame from input sequence of samples
 						List<FeatureVector> frameFV = getFrame(featureVectorList);
 
 						if (DEBUG_MOTION_DETECTION) {
-							System.out.printf("Number of samples in featureVectorList is %d \n", featureVectorList.size());
+							System.out.printf("Number of samples in featureVectorList is %d \n",
+									featureVectorList.size());
 						}
 						// Determine whether a motion or not
 						if (true == motionDetection(frameFV)) {
 							if (DEBUG_MOTION_DETECTION) {
 								System.out.printf("*****Motion detected! \n");
 							}
-							
-							//create a list to store frames which has activity motion.
+
+							// create a list to store frames which has activity
+							// motion.
 							motionFramesList = new LinkedList<List<FeatureVector>>();
 							motionFramesList.add(frameFV);
-							
+
 							state = FIND_END_FRAME;
 						} else {
 							state = MOTION_DETECTION;
@@ -195,14 +344,16 @@ public class MotionRecognition extends Thread {
 					break;
 				case FIND_END_FRAME:
 					if (featureVectorList.size() >= 64) {
-						//get a frame from input sequence of samples
+						// get a frame from input sequence of samples
 						List<FeatureVector> frameFV = getFrame(featureVectorList);
 
 						if (DEBUG_MOTION_DETECTION) {
-							System.out.printf("Number of samples in featureVectorList is %d \n", featureVectorList.size());
+							System.out.printf("Number of samples in featureVectorList is %d \n",
+									featureVectorList.size());
 						}
 
-						// Determine whether the end-point(frame) of motion or not
+						// Determine whether the end-point(frame) of motion or
+						// not
 						if (true == motionDetection(frameFV)) {
 							if (DEBUG_MOTION_DETECTION) {
 								System.out.printf("*****Motion detected! \n");
@@ -210,7 +361,7 @@ public class MotionRecognition extends Thread {
 							motionFramesList.add(frameFV);
 							state = FIND_END_FRAME;
 						} else {
-							//the end-point(frame) of motion found.
+							// the end-point(frame) of motion found.
 							if (DEBUG_MOTION_DETECTION) {
 								System.out.printf("*****Motion Terminate! \n");
 							}
@@ -221,7 +372,7 @@ public class MotionRecognition extends Thread {
 					break;
 
 				case ANALYSIS_FRAMES:
-					//print all samples of the activity motion frames
+					// print all samples of the activity motion frames
 					if (DEBUG_MOTION_DETECTION) {
 						System.out.printf("Number of frame is %d \n", motionFramesList.size());
 						for (List<FeatureVector> fv : motionFramesList) {
@@ -234,23 +385,25 @@ public class MotionRecognition extends Thread {
 						}
 					}
 
-					//Step for training phase, which write the activity motion frames into a file, it is used as motion-template.
+					// Step for training phase, which write the activity motion
+					// frames into a file, it is used as motion-template.
 					if (TRAINING_PHASE) {
-						//Write activity motion frames into a file.
+						// Write activity motion frames into a file.
 						FileOutputStream fos = new FileOutputStream("./dataset/motion_" + String.valueOf(frameIndex));
 						ObjectOutputStream oos = new ObjectOutputStream(fos);
 						oos.writeObject(motionFramesList);
 						oos.close();
 						fos.close();
-						
+
 						if (DEBUG_MOTION_DETECTION) {
-							//Read activity motion frames from the file to check whether store correctly.
+							// Read activity motion frames from the file to
+							// check whether store correctly.
 							FileInputStream fis = new FileInputStream("./dataset/motion_" + String.valueOf(frameIndex));
 							ObjectInputStream ois = new ObjectInputStream(fis);
 							List<List<FeatureVector>> mList = (List<List<FeatureVector>>) ois.readObject();
 							ois.close();
 							fis.close();
-							System.out.printf("[Read from file] \n");						
+							System.out.printf("[Read from file] \n");
 							System.out.printf("Number of frame is %d \n", mList.size());
 							for (List<FeatureVector> fv : mList) {
 								for (int i = 0; i < fv.size(); i++) {
@@ -261,8 +414,22 @@ public class MotionRecognition extends Thread {
 								System.out.println("\n");
 							}
 						}
-
 					}
+					state = MOTION_RECOGNITION;
+					break;
+				case MOTION_RECOGNITION:
+					if (DEBUG_IDENTIFY_MOTION) {
+						System.out.println("========================");
+					}
+					identifyMotion("shake_v");
+					identifyMotion("shake_v1");
+					identifyMotion("shake_h");
+					identifyMotion("shake_h1");
+					identifyMotion("sway");
+					identifyMotion("sway1");
+					if (DEBUG_IDENTIFY_MOTION) {
+						System.out.println("========================");
+					}					
 					state = MOTION_DETECTION;
 					break;
 				}
@@ -271,7 +438,7 @@ public class MotionRecognition extends Thread {
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			} catch (FileNotFoundException e) {
-				e.printStackTrace();
+				System.out.println("[Read file fail] ");
 			} catch (IOException e) {
 				e.printStackTrace();
 			} catch (ClassNotFoundException e) {
